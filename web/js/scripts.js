1d3c51cc741b1551dd1b0fbdca568b1426100ebc
MOHAMMED SAIF <zaifrocks786@gmail.com>
2022-05-23 23:55:52 +0530

    update

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,11 +1,4 @@
-/*!
-* Start Bootstrap - Agency v7.0.6 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+
 
 window.addEventListener('DOMContentLoaded', event => {
 
